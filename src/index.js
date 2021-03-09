a9986a28a899cc5ad19d610d8abb6f3cee702380
Derek Maxson <dmaxson@frontporch.com>
2021-03-09 10:37:20 -0800

    Change root to /chathost/  (from /api/)

--- src/index.js
+++ src/index.js
@@ -4,22 +4,23 @@ const express = require('express'),
     nocache = require('nocache'),
     swaggerUi = require('swagger-ui-express'),
 	moment = require('moment-timezone'),
-    configs = require('./configs.js')
+    configs = require('./configs.js'),
+	swaggerDocument = require('./swagger.json')
 
 
 
 webapp.listen(configs.port);
-//webapp.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));  // TODO
+webapp.use('/chathost/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));  // TODO
 webapp.use(bodyParser.urlencoded({ extended: false }));
 webapp.use(bodyParser.json({ type: 'application/json', limit: '50mb' }));
 webapp.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 webapp.use(bodyParser.text({ type: 'text/html' }));
 webapp.use(nocache());
 
-webapp.use('/api/messageStatus', require('./routes/messageStatus.js'));
-webapp.use('/api/courseRosters', require('./routes/courseRosters.js'));
-webapp.use('/api/messages', require('./routes/messages.js'));
-webapp.use('/api/attachments', require('./routes/attachments.js'));
+webapp.use('/chathost/messageStatus', require('./routes/messageStatus.js'));
+webapp.use('/chathost/courseRosters', require('./routes/courseRosters.js'));
+webapp.use('/chathost/messages', require('./routes/messages.js'));
+webapp.use('/chathost/attachments', require('./routes/attachments.js'));
 
 //webapp.use('/', express.static('www/'));
 
