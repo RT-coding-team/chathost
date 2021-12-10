bf42301eeed625c9f1c764990d34c912edab2bea
Derek Maxson <dmaxson@frontporch.com>
2021-12-10 07:58:39 -0800

    Generate UUID for Rocketchat password

--- src/routes/courseRosters.js
+++ src/routes/courseRosters.js
@@ -5,11 +5,12 @@ const express = require('express'),
     rocketchat = require('../rocketchat.js'),
     Logger = require('../logger.js'),
    	moment = require('moment-timezone'),
-    logger = new Logger(configs.logging);
+    logger = new Logger(configs.logging),
+    { v4: uuidv4 } = require('uuid');
 
 //  Put in the courseRoster data
 router.post('/', async function postRosters(req, res) {
-	req.body[0].authorization = req.boxauthorization;
+	req.body[0].authorization = req.headers.authorization;
 	mongo.setCourseRoster(req.boxid,req.body,req.headers['x-forwarded-for'] || req.socket.remoteAddress, async function(result) {
 		logger.log('debug', `boxId: ${req.boxid}: ${req.method} ${req.originalUrl}: ${result}`);
 		await processRosters(req.boxid,req.body);
@@ -41,7 +42,7 @@ async function processRosters(boxid,body,ip) {
 			logger.log('debug', `boxId: ${boxid}: processRosters: Student: ${username}`);
 			var user = await rocketchat.getUser(boxid,username);
 			if (!user || !user.username) {
-				user = await rocketchat.createUser(boxid,{username:username,email:`${username}@none.com`,password:username,name:`${student['first_name']} ${student['last_name']}`,customFields:{wellId:boxid}})
+				user = await rocketchat.createUser(boxid,{username:username,email:`${username}@none.com`,password:uuidv4(),name:`${student['first_name']} ${student['last_name']}`,customFields:{wellId:boxid}})
 				var chat = await rocketchat.createChat(boxid,[username,teachers[0]]);
 				var welcome = await rocketchat.sendMessage(boxid,username,teachers[0],`You have a new student in ${course['course_name']} at ${boxid}: ${student['first_name']} ${student['last_name']} (${username})`);
 			}
